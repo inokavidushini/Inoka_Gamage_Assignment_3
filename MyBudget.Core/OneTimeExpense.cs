a63b20def893f7596f340c00bb245f144a1c0d46
inoka <inokavidushini@gmail.com>
2026-07-08 11:48:18 -0400

    Add OneTimeExpense

---
+++ MyBudget.Core/OneTimeExpense.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyBudget.Core;
+
+    public record OneTimeExpense(
+    Guid Id,
+    string Description,
+    decimal Amount,
+    ExpenseCategory Category,
+    DateOnly Date
+    )
+{
+    internal class OneTimeExpense
+    {
+    }
+}
