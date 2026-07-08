4fbd5c75f93051168db495bd8b17aea7771c13da
inoka <inokavidushini@gmail.com>
2026-07-08 15:28:06 -0400

    validate description time freequent and money

--- MyBudget.Core/ExpenseFactory.cs
+++ MyBudget.Core/ExpenseFactory.cs
@@ -3,8 +3,61 @@ using System.Collections.Generic;
 using System.Text;
 
 namespace MyBudget.Core
+public static class ExpenseFactory
 {
-    internal class ExpenseFactory
+    public static decimal ValidateAmount(decimal amount)
     {
+        if (amount <= 0 || amount > 1000000)
+            throw new InvalidExpenseException(
+                "Invalid amount");
+
+        return Math.Round(amount, 2);
     }
+
+    public static OneTimeExpense CreateOneTime(
+        string description,
+        decimal amount,
+        ExpenseCategory category,
+        DateOnly date)
+
+    {
+        if (string.IsNullOrWhiteSpace(description))
+            throw new InvalidExpenseException(
+                "Description required");
+
+        description = description.Trim();
+
+        amount = ValidateAmount(amount);
+
+        return new OneTimeExpense(
+           Guid.NewGuid(),
+           description,
+           amount,
+           category,
+           date);
+
+    }
+        public static RecurringExpense CreateRecurring(
+        string description,
+        decimal amount,
+        ExpenseCategory category,
+        DateOnly date,
+        int timesPerMonth)
+    {
+        if (string.IsNullOrWhiteSpace(description))
+            throw new InvalidExpenseException(
+                "Description required");
+
+        if (timesPerMonth < 1)
+            throw new InvalidExpenseException(
+                "Invalid frequency");
+
+
+
+    }
+
+
+
+}
+
 }
