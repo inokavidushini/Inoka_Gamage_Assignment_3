a5d31a7aceb8cd5a41be86647ce8f8280328093c
inoka <inokavidushini@gmail.com>
2026-07-08 17:21:43 -0400

    Save expense to the JOSON file and Conver c# in to JASON text and complete JSON expense store

--- MyBudget.Data/JsonExpenseStore.cs
+++ MyBudget.Data/JsonExpenseStore.cs
@@ -19,4 +19,24 @@ public class JsonExpenseStore : IExpenseStore
         if (!File.Exists(_path))
             return new List<Expense>();
 
+        string json = File.ReadAllText(_path);
+        if (string.IsNullOrWhiteSpace(json))
+            return new List<Expense>();
+
+        return JsonSerializer.Deserialize<List<Expense>>(json)
+               ?? new List<Expense>();
     }
+
+    public void Save(IEnumerable<Expense> expenses)
+    {
+        var options = new JsonSerializerOptions
+        {
+            WriteIndented = true
+        };
+        string json =
+            JsonSerializer.Serialize(expenses, options);
+
+        File.WriteAllText(_path, json);
+    }
+
+}
