806dee703da1c64aa43911b1bacba79d38780784
inoka <inokavidushini@gmail.com>
2026-07-08 11:25:04 -0400

    Add Expenses

---
+++ MyBudget.Core/Expense.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace MyBudget.Core;
+
+[JsonPolymorphic(TypeDiscriminatorPropertyName = "kind")]
+[JsonDerivedType(typeof(OneTimeExpense), "onetime")]
+[JsonDerivedType(typeof(RecurringExpense), "recurring")]
+
+public abstract record Expense(
+    Guid Id,
+    string Description,
+    decimal Amount,
+    ExpenseCategory Category,
+    DateOnly Date
+) : IReportable
+
+{
+    internal class Expense
+    {
+    }
+}
