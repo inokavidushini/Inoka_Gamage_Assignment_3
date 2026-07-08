87288d59ceb8971cb9a5727023b418a619e327f9
inoka <inokavidushini@gmail.com>
2026-07-08 11:56:28 -0400

    Add Recoring Expenses

---
+++ MyBudget.Core/RecurringExpense.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyBudget.Core;
+
+    public record RecurringExpense(
+    Guid Id,
+    string Description,
+    decimal Amount,
+    ExpenseCategory Category,
+    DateOnly Date,
+    int TimesPerMonth
+) : Expense(Id, Description, Amount, Category, Date)
+
+{
+    public override decimal MonthlyImpact =>
+       Amount * TimesPerMonth;
+
+
+}
