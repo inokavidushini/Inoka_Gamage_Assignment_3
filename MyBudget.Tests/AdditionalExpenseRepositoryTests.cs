c015912c5b34e355a1b34ded3365e94e2eed657b
inoka <inokavidushini@gmail.com>
2026-07-08 20:51:41 -0400

    Add method to store multiple expenses

--- MyBudget.Tests/AdditionalExpenseRepositoryTests.cs
+++ MyBudget.Tests/AdditionalExpenseRepositoryTests.cs
@@ -1,10 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using MyBudget.Core;
+using MyBudget.Data;
+using MyBudget.Tests.Fakes;
+using Xunit;
 
-namespace MyBudget.Tests
+namespace MyBudget.Tests;
+//Xunit test to checks whether the ExpenseRepository can store multiple expenses
+
+public class AdditionalExpenseRepositoryTests
 {
-    internal class AdditionalExpenseRepositoryTests
+    [Fact]
+    public void AddMultipleExpenses_ShouldStoreAllExpenses()
     {
+        // Arrange
+        var store = new InMemoryExpenseStore();
+        var repository = new ExpenseRepository(store);
+
+
     }
 }
+
