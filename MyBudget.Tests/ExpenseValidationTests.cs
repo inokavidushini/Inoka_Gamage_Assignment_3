29e627ef61859c0d154cb249ba1c96ec3801a435
inoka <inokavidushini@gmail.com>
2026-07-08 22:08:05 -0400

    Change the code expense empty description

--- MyBudget.Tests/ExpenseValidationTests.cs
+++ MyBudget.Tests/ExpenseValidationTests.cs
@@ -1,10 +1,51 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using MyBudget.Core;
+using MyBudget.Data;
+using MyBudget.Tests.Fakes;
+using Xunit;
 
-namespace MyBudget.Tests
+namespace MyBudget.Tests;
+
+    public class ExpenseValidationTests
 {
-    internal class ExpenseValidationTests
+    // Xunit tests to check ExpenseRepository behavior
+    [Fact]
+    public void CreateExpense_WithEmptyDescription_ShouldThrowException()
     {
+        /*// Arrange + Act + Assert
+
+        Assert.Throws<InvalidExpenseException>(() =>
+            ExpenseFactory.CreateOneTime(
+                "",
+                10m,
+                ExpenseCategory.Food,
+                default));*/
+        // Arrange
+        string description = "";
+        decimal amount = 10m;
+        ExpenseCategory category = ExpenseCategory.Food;
+
+        //Act
+        Action action = () => ExpenseFactory.CreateOneTime(description, amount, category, default);
+
+        // Assert
+        var Exception = Assert.Throws<InvalidExpenseException>((action));
+        Assert.Equal("Expense description cannot be empty.", Exception.Message);
     }
-}
+       
+    
+    [Fact]
+    public void CreateExpense_WithNegativeAmount_ShouldThrowException()
+    {
+        /*Assert.Throws<InvalidExpenseException>(() =>
+            ExpenseFactory.CreateOneTime(
+                "Coffee",
+                -5m,
+                ExpenseCategory.Food,
+                default));*/
+
+
+    }
+
+ }
+
